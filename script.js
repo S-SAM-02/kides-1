081d32fce511338f01fc1ca1ad5cfaee7e563a85
Santhosh S <rathasenthil194@gmail.com>
2025-07-28 15:09:27 +0530

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const videoLinks = {
   cartoons: [
-    "https://www.youtube.com/embed/2Xc9gXyf2G4",
-    "https://www.youtube.com/embed/0bGjlvukgHU",
-    "https://www.youtube.com/embed/j1VJgB8_Lxg",
-    "https://www.youtube.com/embed/TttM04yVZtM"
+    "https://www.youtube.com/embed/MtN1YnoL46Q", // Peppa Pig - The Balloon Ride
+    "https://www.youtube.com/embed/2S__fbCGwOM", // Masha and The Bear - Recipe for Disaster
+    "https://www.youtube.com/embed/9xv-FjbXaqk", // Paw Patrol - Pups Save the Penguins
+    "https://www.youtube.com/embed/1jv0G6w5BzY"  // Tom & Jerry - Classic Cartoon Compilation
   ],
   rhymes: [
-    "https://www.youtube.com/embed/j02C2G4dTMo",
-    "https://www.youtube.com/embed/U7zS3kLBRzo",
-    "https://www.youtube.com/embed/zJ1f0OnEqlQ",
-    "https://www.youtube.com/embed/fHI8X4OXluQ"
+    "https://www.youtube.com/embed/Jr8l8p2WkZk", // Wheels on the Bus
+    "https://www.youtube.com/embed/yCjJyiqpAuU", // Baby Shark Dance
+    "https://www.youtube.com/embed/75p-N9YKqNo", // Johny Johny Yes Papa
+    "https://www.youtube.com/embed/0peZ5AN5vs8"  // Five Little Ducks
   ],
   learning: [
-    "https://www.youtube.com/embed/1I5ZMmrOfnA",
-    "https://www.youtube.com/embed/hRLukslv74M",
-    "https://www.youtube.com/embed/qWc7vPb5jUo",
-    "https://www.youtube.com/embed/vbKHiJ3jPEI"
+    "https://www.youtube.com/embed/h0Kp_J9kvNM", // ABC Song
+    "https://www.youtube.com/embed/2Xy2e78qxNg", // Colors Song
+    "https://www.youtube.com/embed/OPzIbbvoiMA", // Shapes Song
+    "https://www.youtube.com/embed/8yteMugRAc0"  // Counting 1-20
   ]
 };
 
@@ -33,5 +33,5 @@ function respond(type) {
     iframe.src = link;
     iframe.allowFullscreen = true;
     videoArea.appendChild(iframe);
-  });
+     });
 }
